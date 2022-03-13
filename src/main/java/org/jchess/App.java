096ce75127c4ff39278742590bd77111536d25ab
Arian Dannemann <arian.dannemann@gmail.com>
2022-03-13 16:44:24 +0100

    Made app build ready

--- src/main/java/org/jchess/App.java
+++ src/main/java/org/jchess/App.java
@@ -1,9 +1,5 @@
 package org.jchess;
 
-import org.jchess.control.BoardManager;
-import org.jchess.model.Board;
-import org.jchess.view.UI;
-
 /**
  * This class is used to show a simple example of the JChess libary
  *
@@ -14,7 +10,6 @@ public class App
 {
     public static void main (String[] args)
     {
-        Board board = BoardManager.generateBoard();
-        UI.printBoard(board);
+        ;
     }
 }
